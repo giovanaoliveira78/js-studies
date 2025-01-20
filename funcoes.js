8d5622014bf8b8b5e9dac0064b944819c5c84570
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-01-20 09:30:52 -0300

    teoria funcoes

---
+++ funcoes.js
@@ -0,0 +1,4 @@
+// São blocos de código que a gente cria pra realizar uma tarefa especifica
+// A gente cria uma função uma vez e pode chamar ela quantas vezes precisar
+// Funções ajudam a deixar o código mais organizado 
+// Torna o código mais reutilizável
