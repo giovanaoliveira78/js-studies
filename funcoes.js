8d07404d0316884a2f1db7c6f930057452f99d63
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-01-21 20:18:22 -0300

    primeira função

--- funcoes.js
+++ funcoes.js
@@ -2,3 +2,10 @@
 // A gente cria uma função uma vez e pode chamar ela quantas vezes precisar
 // Funções ajudam a deixar o código mais organizado 
 // Torna o código mais reutilizável
+
+function message () {
+  console.log("Olá! É bom ter você aqui")
+} /// só dessa forma a função não é executada
+
+message() // é necessário chamar a função para que ela seja executada
+message() // se eu chamar de novo, ela vai executar DE NOVO, e posso chamar quantas vezes eu quiser
