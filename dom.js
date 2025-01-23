517794b28c744ea4406f7e691b2058686fa668e6
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-01-23 10:53:59 -0300

    DOM teoria

---
+++ dom.js
@@ -0,0 +1,6 @@
+// Document object Model - DOM
+
+/*
+  É a representação de dados dos objetos que compõem a estrutura e o conteúdo de um documento na web, (uma página HTML é um documento.)
+  Representa o documento com nós e objetos (estrutura de árvore) que pode ser acessado e modificado.
+*/
