b943308763c9890e3a2a26e835e3750d9d729f06
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-02-01 19:56:20 -0300

    objetos

---
+++ intermediate-js-studies/objetos.js
@@ -0,0 +1,22 @@
+// Objetos
+
+// Um objeto é uma estrutura que representa algo com propriedades e comportamentos
+
+// Propriedades
+
+// Imagine que você tem um carro
+// O carr é um objeto
+// Ele tem várias caracteristicas (PROPRIEDADES)
+
+// Então podemos criar um objeto CARRO, e dentro do objeto CARRO podemos definir as caracteristicas dele 
+// Caracteristicas do carro: modelo, número de portas, ano de fabricação, cores diferentes
+
+// Definimos dentro do objeto propriedades para representá-lo
+
+// Métodos
+
+// método é o comportamento
+// Além de características (propriedades), o carro pode fazer coisas (comportamentos)
+// Um carro pode ligar/desligar, acelerar/frear etc
+// Tudo que um objeto executa é chamado de comportamento
+// no Javascript as propriedades podem ser valores, como texto ou números, e os métodos são funções associadas a esse objeto
