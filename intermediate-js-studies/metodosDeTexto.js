c00066bedfefa4f55618ebe3c7b579386a50a729
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-02-05 07:31:22 -0300

    exibe o conteudo da string em maiusculo ou minusculo

---
+++ intermediate-js-studies/metodosDeTexto.js
@@ -0,0 +1,11 @@
+/// Exibir o conteúdo da string em maiusculo ou minusculo
+
+let message = "Estou estudando os fundamentos do Javascript."
+
+console.log(message)
+
+// Exibir em maiusculo
+console.log(message.toUpperCase())
+
+// Exibir em minusculo
+console.log(message.toLowerCase())
