2dc993723a1a7998b3753619cfa8906b01832d69
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-02-05 08:18:12 -0300

    substitui e fatia um texto

--- intermediate-js-studies/metodosDeTexto.js
+++ intermediate-js-studies/metodosDeTexto.js
@@ -26,3 +26,36 @@ console.log(typeof value) // typeof pra visualizar o tipo, nesse caso number
 console.log(value.length) // Se eu usar lenght pra number vai dar undefined
 console.log(String(value).length) // Então é necessário converter para uma string
 console.log(value.toString().length) // Dessa forma também converte pra string
+
+/// Como substituir parte de um texto
+let message1 = "Hey, girl"
+
+console.log(message1.replace("Hey", "Hi")) // O método replace contém dois parametros que são duas strings
+// o primeiro parametro é o que eu quero substituir 
+// o segundo é pelo que eu quero substituir
+
+
+// Também da pra trocar frases
+
+let message2 = "Hey, I'm a software quality analyst"
+console.log(message2) // mensagem original
+console.log(message2.replace("software quality analyst", "Quality Assurance Engineer")) // Mensagem alterada
+
+// Como extrair uma parte da string (start, end) sinalizando onde começa e onde termina
+
+console.log(message2.slice(0,3)) // dessa forma ele pega da posição zero, até a posição 3, e irá retornar o "Hey"
+console.log(message2.slice(11,19)) // pega da posição 11 até a 19 e exibe "software"
+
+// Como extrair uma parte da string de trás pra frente
+console.log(message2.slice(-7)) // nesse caso eu conto quantos caracteres eu tenho na última palavra e uso o negativo (-7)
+// usando o negativo ele pega de trás pra frente, ou seja começando pela última palavra
+
+
+// Método para remover espaços em braco no início ou final da string
+let textWithSpace = "     Texto de exemplo    "
+
+console.log(textWithSpace.length) // aqui eu verifico o tamanho da string
+console.log(textWithSpace.trim()) // trim remove espaços em branco no início e no final da string, retornou sem os espaços
+console.log(textWithSpace.trim().length) // aqui eu consigo verificar o tamanho que tem agora depois de remover os espaços
+
+// o método TRIM não vai remover os espaços do meio do texto, pois ele entende que faz parte da string, irá remover só o espaço do começo e fim
