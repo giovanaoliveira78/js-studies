997de6897193a6b3c9296cb0b0b6639e5abde9a7
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-02-06 17:55:45 -0300

    cria um array com construtor

---
+++ intermediate-js-studies/array.js
@@ -0,0 +1,18 @@
+// Array é uma coleção ordenada de valores
+// Podemos comparar um array a uma lista, onde cada item da lista possui uma posição específica conhecida como índice.
+
+// Criar Array com construtor
+
+const newArray = new Array()
+console.log(newArray)
+
+// Retorna o tamanho do array
+console.log("Stefan".length)
+
+// [] - Array
+// {} Objeto
+
+// Criar o array com 10 posições VAZIAS.
+const available = new Array(10)
+console.log(available)
+console.log(available.length)
