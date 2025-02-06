ff6d2c895734f46c09e88179ef9f800ca242b78f
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-02-06 18:15:33 -0300

    cria e acessa um array

--- intermediate-js-studies/array.js
+++ intermediate-js-studies/array.js
@@ -16,3 +16,19 @@ console.log("Stefan".length)
 const available = new Array(10)
 console.log(available)
 console.log(available.length)
+
+// Criando e acessando um array
+
+// Cria nosso array
+let fruits = ['Apple', 'banana', 'orange']
+console.log(fruits)
+
+// verifica o tamanho
+console.log(fruits.length)
+
+// Acessa o item pelo índice
+console.log(fruits[1])
+
+// Exibe o último elemento da lista
+console.log(fruits[fruits.length -1 ]) // minha lista tem o tamanho 3, mas como são indices, é 0, 1, 2. E dessa forma vai ser undefined, então SEMPRE é o tamanho da lista -1
+// sempre é o tamanho da lista -1
