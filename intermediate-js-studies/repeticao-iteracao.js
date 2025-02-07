2a7abf68652c4054207f77834c2b50770ac3e633
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-02-07 18:15:22 -0300

    repeticao while

---
+++ intermediate-js-studies/repeticao-iteracao.js
@@ -0,0 +1,20 @@
+// Repetições
+
+// Executa um bloco de código várias vezes de acordo com uma condição específica
+
+// Lop while
+
+// WHILE: executa até que a condição seja verdadeira
+
+let execute = true
+
+while (execute) {
+  let response = window.prompt("Deseja continuar: 1 (SIM) ou 2 (NÃO)")
+
+  console.log(response)
+  if (response === "2") {
+    execute = false
+  }
+}
+
+console.log("Segue o fluxo...")
