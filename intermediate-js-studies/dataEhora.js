3192b86e3653e451fd341799cab6da5a5dd7d26b
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-02-10 08:00:23 -0300

    define uma data e hora específica

--- intermediate-js-studies/dataEhora.js
+++ intermediate-js-studies/dataEhora.js
@@ -6,3 +6,14 @@ console.log(new Date(0))
 
 // Exibe o número de milissegundos
 console.log(new Date().getTime())
+
+// Define uma data com ano, mês e dia
+console.log(new Date(2025, 6, 3)) // Os meses sempre começam do zero, 0, 1, 2...
+
+// Define data, hor (ano, mês e dia), hora, minuto e segundo
+console.log(new Date(2025, 1, 10, 7, 52, 0))
+
+// Define data e hora com string
+console.log(new Date("2025-02-10T07:57:00")) // quando passa em string o mês segue a sequência normal de 1, 2 ,3.....
+
+console.log(new Date("February 10 2025 07:58:00"))
