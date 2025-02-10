a7d8e9381ca8237e4d8dfa774b95d6ad78744ec3
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-02-10 08:14:13 -0300

    metodos para trabalhar com data e hora

--- intermediate-js-studies/dataEhora.js
+++ intermediate-js-studies/dataEhora.js
@@ -17,3 +17,31 @@ console.log(new Date(2025, 1, 10, 7, 52, 0))
 console.log(new Date("2025-02-10T07:57:00")) // quando passa em string o mês segue a sequência normal de 1, 2 ,3.....
 
 console.log(new Date("February 10 2025 07:58:00"))
+
+///////////////////////////////////////////////
+
+let date = new Date("2025-02-10T08:02:00")
+
+// Dia da semana contando de 0 a 6, domingo sendo 0, 1 segunda, 2 terça...
+console.log(date.getDay())
+
+// Exibe dia do mês, também conta de 0 a 30
+console.log(date.getDate())
+
+// Exibe o mês, de 0 a 11
+console.log(date.getMonth())
+
+// Se eu quiser fazer do jeito "tradicional", com os meses de 1 a 12, eu posso colocar o + 1, pra ficar "certo"
+console.log(date.getMonth() + 1)
+
+// Exibe o ano
+console.log(date.getFullYear())
+
+// Exibe a hora
+console.log(date.getHours())
+
+// Exibe os minutos
+console.log(date.getMinutes())
+
+// Exibe os segundos
+console.log(date.getSeconds())
