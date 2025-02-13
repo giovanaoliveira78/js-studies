6f65c8e4d115282e60e87a83ffcd3ab09efc5c65
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-02-13 17:27:16 -0300

    conceito de classes

---
+++ intermediate-js-studies/classes.js
@@ -0,0 +1,13 @@
+// Classes
+
+// No JS as classes são uma forma de criar objetos e definir comportamento por meio de construtores e métodos.
+// Elas foram introduzidas no ECMAScript 2015 (também conhecido como ES6) para fornecer uma sintaxe mais amigável para a criação de objetos e herança de protótipos (sintax sugar)
+
+// Construtores e métodos
+
+// Uma classe é basicamente um modelo para criar objetos. Ela tem um construtor, que é um método especial chamado quando um objeto é instanciado a partir da classe.
+// Além do construtor, você pode adicionar métodos a uma classe. Métodos são funções associadas a objetos e descrevem o comportamento desses objetos.
+
+// Herança
+
+// Uma classe pode herdar propriedades e métodos de outra classe, permitindo a reutilização do código.
