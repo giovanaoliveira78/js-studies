fffa2abc5bb3bfbe0436bda092c24e886d15726c
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-02-14 12:43:39 -0300

    cria propriedades dentro das classes

--- intermediate-js-studies/classes.js
+++ intermediate-js-studies/classes.js
@@ -19,3 +19,18 @@ class Person {
 }
 
 const person = new Person("Tito")
+
+///////////////////////////////////////////
+
+// Criando propriedades dentro das classes
+class Product {
+  constructor(name) {
+    this.name = name
+  }
+}
+
+const product1 = new Product("Teclado")
+console.log(product1.name)
+
+const product2 = new Product("Mouse")
+console.log(product2.name)
