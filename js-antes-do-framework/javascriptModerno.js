9a7f3f30f0a35db4b37f171f10ae894ecfacecd2
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-03-09 20:43:33 -0300

    strict mode

--- js-antes-do-framework/javascriptModerno.js
+++ js-antes-do-framework/javascriptModerno.js
@@ -15,3 +15,43 @@
 // As versões do ECMAScript (ES6, ES7, etc) indicam as atualizações novidades a linguagem ao longo do tempo.
 // Sempre que há uma nova atualização dos recursos do JavaScript, ela é lançada pelo ECMAScript, que pode ser abreviada como ES.
 // É dessa especificação que veio siglas como ECMAScript 2023, ES2023 ou ES14.
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// Strict Mode
+
+// O strict mode (modo estrito): ativando esse modo, os erros que eram silenciosos passa a gerar exceçoes no JS
+
+"use strict" // Não irá permitir o uso de variáveis sem declaração, irá retornar um erro
+// Da pra usar global e dentro da função
+function showMessage() {
+
+  let personName = "Jhonny" // se colocar a let não irá dar erro, pois ele vai procurar a variável no escopo global
+  console.log("Olá, " + personName)
+}
+
+showMessage()
+
+class Student {
+  get point() { // get point é uma função para obter um retorno
+    return 7
+  }
+}
+
+let Student = new Student()
+// Tenta mudar uma propriedade somente leitura
+// student.point = 10
+
+console.log(student.point)
+
+// Tentando deletar uma propriedade de um objeto que não posso deletar
+//delete window.document
+
+// Quando passamos parametros duplicados.
+/*function sum(a, a, c) {
+  return a + a + c
+}
+
+const result = sum(1, 3, 2)
+console.log("RESULTADO", result)
+*/
