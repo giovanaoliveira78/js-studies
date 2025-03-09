87402e3630fd80a0a701d813fc78cec7c580ef09
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-03-08 22:37:32 -0300

    ecmascript

---
+++ js-antes-do-framework/javascriptModerno.js
@@ -0,0 +1,15 @@
+// ECMAScript
+
+// O JavaScript foi criado foi criado em 1995 por Brendan Eich, enquanto ele trabalhava no Netscape.
+// O nome original da linguagem era Mocha, que ainda foi mudado para LiveScript, antes de a linguagem ser de fato nomeada como JavaScript.
+
+// ECMA
+
+// No ano de 1996, com o objetivo de padronizar a linguagem a Netspace decidiu juntar o JavaScript a ECMA International.
+// A ECMA (Associação Europeia dos Fabricantes de Computadores) é uma associação que realiza a especificação e padronização de sistemas da informação.
+
+// ECMAScript
+
+// Com essa junção, foi criado um novo padrão de linguagem, que foi batizado com a união das palavras "ECMA" e "JavaScript", resultando no nome ECMAScript.
+// Em resumo, JS é a linguagem que você escreve e executa nos navegadores, enquanto ECMAScript é a especificação que define como a linguagem JS deve se comportar.
+// As versões do ECMAScript (ES6, ES7, etc) indicam as atualizações novidades a linguagem ao longo do tempo.
