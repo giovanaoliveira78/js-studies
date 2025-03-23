544b62cfb53efec43970248e432c7f64e3415db3
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-03-23 19:56:24 -0300

    metodo findIndex()

--- js-antes-do-framework/metodosDeArray.js
+++ js-antes-do-framework/metodosDeArray.js
@@ -46,3 +46,20 @@ const produtos = [
 // Exemplo de um filtro de produtos na promoção
 const promotion = produtos.filter((product) => product.promotion == true)
 console.log(promotion)
+
+/////////////////////////////////////////////////////
+
+// Metodo findIndex()
+
+// retorna o indice no array do primeiro elemento que satisfaz a condição. Caso contrario, retorna -1, indicando que nenhum elemento passou no teste
+
+const values = [4, 6, 8, 12]
+
+// Obtendo o primeiro indice do elemento que o valor é maior do que quatro
+const index = values.findIndex((value) => value > 4)
+
+console.log(index)
+console.log(values[index])
+
+// Exemplo de quando não encontrou 
+console.log(values.findIndex((value) => value > 12))
