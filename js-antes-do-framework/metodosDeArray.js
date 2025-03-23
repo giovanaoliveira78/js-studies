ccc3d9c7e8f0a6e6dcba59fa96552cf1c5723a45
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-03-23 19:47:54 -0300

    metodo filter

--- js-antes-do-framework/metodosDeArray.js
+++ js-antes-do-framework/metodosDeArray.js
@@ -16,11 +16,33 @@ products.map(product => console.log(product)) // se for só pra retornar, da pra
 const formatted = products.map((product) => {
   //return product.toUpperCase()
 
-  return {
-    id: Math.random(),
-    description: product,
-    price: 150
-  }
+  /* return {
+     id: Math.random(),
+     description: product,
+     price: 150
+   }*/
 })
 
 console.log(formatted)
+
+/////////////////////////////////////////////////////
+
+// Método FILTER
+
+// O método filter() cria um novo array com todos os elementos que passaram na condição
+
+const words = ["Javascript", "HTML", "CSS", "WEB"]
+
+// Filtrando palavras que tenham mais do que 3 letras
+const result = words.filter((word) => word.length > 3)
+console.log(result)
+
+const produtos = [
+  { description: "Teclado", price: 150, promotion: true },
+  { description: "Mouse", price: 70, promotion: false },
+  { description: "Monitor", price: 900, promotion: true },
+]
+
+// Exemplo de um filtro de produtos na promoção
+const promotion = produtos.filter((product) => product.promotion == true)
+console.log(promotion)
