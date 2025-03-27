7c9377aa5c619f229ced211f272d6550f2c9c0cb
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-03-27 11:21:02 -0300

    aplicando imutabilidade

--- js-antes-do-framework/imutabilidade.js
+++ js-antes-do-framework/imutabilidade.js
@@ -16,3 +16,34 @@
 
 // Benefício
 // Como os dados imutáveis podem determinar facilmente se ocorreram alterações, isso também ajuda a determinar quando um componente deve ser renderizado novamente.
+
+// Aplicando imutabilidade 
+
+const address1 = {
+  street: "Av. Brasil",
+  number: 20
+}
+
+// Isso não é uma cópia, é uma referência
+//const address2 = address1
+//address2.number = 30
+
+// Aqui estamos criando um novo objeto utilizando as propriedades e valores de address1
+// Opção 1:
+//const address2 = { ...address1 }
+//address2.number = 30
+
+
+const address2 = { ...address1, number: 30 }
+console.log(address1, address2)
+
+
+const list1 = ["Apple", "Banana"]
+
+// Exemplo de referencia de array
+// const list2 = list1
+//list2.push("Watermelon") (PUSH é para adicionar)
+
+const list2 = [...list1, "Watermelon"]
+
+console.log(list1, list2)
