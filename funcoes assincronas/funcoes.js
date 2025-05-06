6899e3f4a4ae2acf8f43dbb38369072ba12a1c37
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-05-06 09:55:36 -0300

    funcoes assincronas

---
+++ funcoes assincronas/funcoes.js
@@ -0,0 +1,28 @@
+// setTimeout() executa uma função após um intervalo de tempo especificado
+setTimeout(() => {
+  console.log("Olá! Tudo bem?")
+}, 2000) // Conta o tempo em milisegundos
+
+// setInterval() executa uma função após um intervalo de tempo especificado
+
+let value = 10
+const interval = setInterval(() => {
+  console.log(value)
+  value--
+
+  if (value === 0) {
+    console.log("FELIZ ANO NOVO!!!!!")
+
+    // Interrompe o intervalo de execuções
+    clearInterval(interval)
+  }
+}, 1000) // irá executar a função a cada 1 segundos
+
+// Funções assíncronas 
+/*
+Quando uma função assíncrona é chamada, ela retorna uma Promise
+Quando a função assíncrona retorna um valor, a Promise será resolvida com o valor retornado
+Quando a função assíncrona lança uma exceção ou algum valor, a Promise será rejeitada com o valor lançado
+
+Uma função assíncrona pode conter uma expressão de await, que pausa a execução da função assíncrona e espera pela resolução da Promise passada, e depois retoma a execução da função assíncrona e retorna o valor resolvido.
+*/
