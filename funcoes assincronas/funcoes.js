14761ad85199380b6dc5c60a1c064a154d76ec66
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-05-10 19:50:03 -0300

    conhecendo o event loop

--- funcoes assincronas/funcoes.js
+++ funcoes assincronas/funcoes.js
@@ -88,3 +88,48 @@ fetch()
   asyncFunction()
   console.log(response)
 }*/
+
+// Event loop
+/*
+single threaded:
+Executa uma coisa por vez
+
+no-blocking:
+Não trava o contexto da execução
+
+asynchronous:
+Por ser no-blocking precisa utilizar um paradigma assíncrono para lidar com a execução das coisas
+
+concurrent:
+As tarefas que executam assincronamente concorrem uma com as outras pelo processamento
+
+Concorrência e Event Loop
+O JS possui um modelo de concorrencia baseado em um event loop responsável por gerenciar a execução de código assincrono e eventos em um único thread, permitindo que o JS seja não bloqueante
+
+
+Call Stack
+Armazena as chamadas de funções em execução. Quando uma função é chamada, ela é empilhada no topo da pilha.
+Tudo passa pela call stack, algumas tarefas são executadas nela mesma e outras somente passam por ela e será executada e uma web API
+
+
+WEB API
+Utilização de outros recursos
+
+Callback Queue
+Fila que armazena callbacks e eventos que aguardam para serem processados
+
+Event Loop
+Verifica a pilha de Call Stack e Callback. Se houver, um callback é removido da fila e colocado na pilha de chamadas para execução
+Tudo passa pela call stack. Algumas tarefas serão resolvidas nele mesmo e outras somente irá passar pela Call Stck sem fazer nada e será resolvida em alguma WEB API
+O Event Loop é quem fica checkando constantemente a call stack e callback queue
+
+Micro e Macro tasks
+Microtasks: São tarefas de alta prioridade que são executadas antes das Macrotasks (temporizadores e promises)
+Macrotasks: Sao tarefas de menor prioridade, como callbacks de eventos, setTimeout e setInterval
+
+####  RESUMO:  ####
+1. Execução de código: O código síncrono é executado de cima para baixo, empilhando e desempilhando funções conforme necessário
+2. Eventos assíncronos: Quando ocorrem eventos assíncronos, como uma requisição concluída, o callback correspondente é enfileirado na fila de callback
+3. Verificação do Event Loop: O Event Loop verifica a pilha de chamadas e a fila de callback. Se a pilha estiver vazia, ele move um callback da fila de callback para a pilha de chamadas
+4. Microtasks: Antes de verificar novamente a fila de callback, o Event Loop executa todas as microtasks pendentes
+*/
