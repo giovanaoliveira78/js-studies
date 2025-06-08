ba2fdee9d4fb3c043a9b6bca9a7865a1ad875e0c
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-06-08 18:37:40 -0300

    o que e uma api

---
+++ js-antes-do-framework/workingApi.js
@@ -0,0 +1,21 @@
+// O que é uma API?
+/*
+Uma API (Apllication Programming Interface) é uma Interface que disponibiliza um conjunto de funcionalidades para serem utilizadas.
+Exemplos: API de CEP, previsão do tempo, cotação de moedas
+
+Métodos HTTP
+GET - Leitura (Buscar dados)
+POST - Criação (Criar dados)
+PUT - Atualização (Atualizar dados)
+DELETE - Exclusão (Deletar dados)
+PACTH - Atualização parcial (atualiza somente um dado)
+
+JSON 
+Javascript Object Notation, é uma notação de objetos utilizado para representar dados
+É amplamente utilizado na comunicação entre servidores e clientes
+
+É um objeto, com chave e valor
+{
+  "key": "value"
+}
+*/
